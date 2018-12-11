2dff01188872a71826d6f0128380ff82388fd1b8
mz0 <marcuzero@gmail.com>
2018-12-11 13:55:59 +0300

    cleanup

--- reinit1.py
+++ reinit1.py
@@ -16,15 +16,15 @@ inifile.read("/home/mz0/.ssh/aruba/ansible.ini")
 myId = inifile.get("acct0", "user")
 myPw = inifile.get("acct0", "passwd")
 
-tmp_root_passwd='hjgkhg_bkhjgbkhgb'
-template='debian8_x64_1_0'
-vmname='La'
-dc=1
+tmp_root_passwd = 'hjgkhg_bkhjgbkhgb'
+# template = 'debian8_x64_1_0'
+vmname = 'La'
+dc = 1
 
 i = CloudInterface(dc=dc, debug_level=logging.DEBUG)
 i.login(username=myId, password=myPw, load=False)
 
-vm=i.get_vm(pattern=vmname)[0]
+vm = i.get_vm(pattern=vmname)[0]
 print('Reinitialize: %s' % vm.vm_name)
 if vm.status == 3:
         vm.poweroff()
@@ -34,11 +34,11 @@ vm.reinitialize(admin_password=tmp_root_passwd)
 
 # 2018-12-10 15:19 MainThread DEBUG
 # :28,777 {"ApplicationId":"GetServers",
-# :29,761 {"ApplicationId":"GetPurchasedIpAddresses",  "RequestId":"", "SessionId":"", "Password":"", "Username":""}
+# :29,761 {"ApplicationId":"GetPurchasedIpAddresses", ....}
 # :31,199 {"ApplicationId":"GetServerDetails",
 # Reinitialize: La
-# :32,032 {"ApplicationId":"SetEnqueueServerPowerOff", "RequestId":"", "SessionId":"", "Password":"", "Username":"", "ServerId": 2959}
-# :33,241 {"ApplicationId":"GetJobs",                  "RequestId":"", "SessionId":"", "Password":"", "Username":""}
+# :32,032 {"ApplicationId":"SetEnqueueServerPowerOff",...., "ServerId":_}
+# :33,241 {"ApplicationId":"GetJobs",                 ....}
 # :35,469 {"ApplicationId":"GetJobs",
 # :37,404 {"ApplicationId":"GetJobs",
 # :39,002 {"ApplicationId":"GetJobs",
@@ -46,4 +46,5 @@ vm.reinitialize(admin_password=tmp_root_passwd)
 # :42,716 {"ApplicationId":"GetJobs",
 # :44,480 {"ApplicationId":"GetJobs",
 # :46,197 {"ApplicationId":"GetJobs",
-# :47,189 {"ApplicationId":"SetEnqueueReinitializeServer", "RequestId":"", "SessionId":"", "Password": "", "Username": "", "AdministratorPassword": "", "ServerId": 2959, "ConfigureIPv6": false}
+# :47,189 {"ApplicationId":"SetEnqueueReinitializeServer",....,
+#          "AdministratorPassword":"~", "ServerId":_, "ConfigureIPv6":false}
