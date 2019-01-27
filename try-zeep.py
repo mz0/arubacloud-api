f557994009a2d60e19a61d137d0f0e3fb16f2bbf
mz0 <marcuzero@gmail.com>
2019-01-27 17:27:18 +0300

    fix auth, add working WS-API example

---
+++ try-zeep.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+#import logging.config
+#logging.config.dictConfig({
+#    'version': 1,
+#    'formatters': {
+#        'verbose': {
+#            'format': '%(name)s: %(message)s'
+#        }
+#    },
+#    'handlers': {
+#        'console': {
+#            'level': 'DEBUG',
+#            'class': 'logging.StreamHandler',
+#            'formatter': 'verbose',
+#        },
+#    },
+#    'loggers': {
+#        'zeep.transports': {
+#            'level': 'DEBUG',
+#            'propagate': True,
+#            'handlers': ['console'],
+#        },
+#    }
+#})
+
+from configparser import ConfigParser as iniParser
+
+inifile = iniParser()
+inifile.read("/home/mz0/.ssh/aruba/ansible.ini")
+myId = inifile.get("acct0", "user")
+myPw = inifile.get("acct0", "passwd")
+
+from zeep import Client
+from zeep.wsse.username import UsernameToken
+
+wsdl_uri='https://api.dc1.computing.cloud.it/WsEndUser/v2.9/WsEndUser.svc?wsdl'
+client = Client(wsdl_uri, wsse=UsernameToken(myId, myPw))
+templates = client.service.GetHypervisors()
+
+print(templates)
